c60fbbe1908cf99e5332a646bb10f298b3d14f43
EfVelasquez <efvb1@alu.ua.es>
2025-02-17 17:37:32 +0100

    Spring initializr base project and postgres integration with docker

---
+++ app/src/main/java/com/weatherstations/WeatherStations/WeatherStationsApplication.java
@@ -0,0 +1,13 @@
+package com.weatherstations.WeatherStations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherStationsApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/weatherstations/WeatherStations/WeatherStationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherstations.WeatherStations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherStationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
